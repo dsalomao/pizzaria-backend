a30edd7d179e9a0a6a94a8deb6ba102cbb40ec67
Daniel E. Salomão <dsalomao@users.noreply.github.com>
2023-12-12 17:35:12 -0300

    Adding the category list and create routes

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,9 @@ import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
 const router = Router()
 
@@ -11,5 +14,12 @@ const router = Router()
 router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+// ROTAS CATEGORY
+router.post('/categories', isAuthenticated, new CreateCategoryController().handle)
+router.get('/categories', isAuthenticated, new ListCategoryController().handle)
+
+// ROTAS PRODUCT
+router.post('/products', isAuthenticated, new CreateProductController().handle)
 
 export  { router }
